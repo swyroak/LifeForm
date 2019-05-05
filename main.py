4631773be2cc994456e5e3d6de741abc29522cd7
swyroak <947x9i@bma.biglobe.ne.jp>
2019-05-05 22:07:31 +0900

    aaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from createfield import Field
+from createfield import ConstFiled
 from lifecycle import basecycle
 from changetostring import ChangeToString
 app = Flask(__name__)
@@ -8,13 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    basefield = Field()
+    basefield = ConstFiled(50, 50)
     cycles = []
     bufcycles = []
     buffiled = basecycle(basefield.get_room())
     cycles.append(buffiled)
     bufcycles.append(ChangeToString(cycles[0]).get_stringfiled())
-    for i in range(5):
+    for i in range(300):
         cycles.append(basecycle(cycles[i]))
         bufcycles.append(ChangeToString(cycles[i]).get_stringfiled())
     print(bufcycles)
@@ -25,3 +25,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.run('0.0.0.0',port=8080)
