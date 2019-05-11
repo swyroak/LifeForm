2c421626cd404f49ac54ae4714e5ea387709ad4a
swyroak <947x9i@bma.biglobe.ne.jp>
2019-05-12 02:47:42 +0900

    GIF化完了

--- imaging.py
+++ imaging.py
@@ -1,11 +1,12 @@
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageDraw
 from createfield import Field
-import os
+# import os
+# import tempfile
+# import random
 
 
 class ImagingObjct():
     def __init__(self, fieldS, height, width):
-        os.mkdir('dev1111')
         self.__height = height * 6 + 1
         self.__Width = width * 6 + 1
         self.__img = Image.new('RGB', (self.__Width, self.__height))
@@ -25,8 +26,14 @@ class ImagingObjct():
             i = 1
             j += 6
         # self.__img.show()
+        # savename = 'dev' + str(random.randrange(1, 100000))
+        # self.__img.save(savename + '.jpg', quality=50)
 
+    def sss(self):
+        return self.__img
 
 if __name__ == "__main__":
     fld = Field()
     objimage = ImagingObjct(fld.get_room(), fld.get_height(), fld.get_width())
+    h=objimage.sss()
+    h.show()
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from changetostring import ChangeToString
 from lifecycle import basecycle
 from createfield import ConstFiled
 from flask import render_template
 from flask import Flask
-import numpy as np
+from imaging import ImagingObjct
+from PIL import Image, ImageDraw
+
 app = Flask(__name__)
 
 
@@ -11,23 +12,27 @@ app = Flask(__name__)
 def hello():
     basefield = ConstFiled(50, 50)
     cycles = []
-    strcycles = []
     arycycles = []
+    images = []
+
     buffiled = basecycle(basefield.get_room())
+    height = basefield.get_height()
+    width = basefield.get_width()
     cycles.append(buffiled)
-    strcycles.append(ChangeToString(cycles[0]).get_stringfiled())
+    img = Image.new('RGB', (height, width))
+    images.append(ImagingObjct(buffiled, height, width).sss())
     for i in range(300):
         bufcycle = basecycle(cycles[i])
         bufarycycle = bufcycle.tolist()
-        bufstrcycle = ChangeToString(bufcycle).get_stringfiled().tolist()
         cycles.append(bufcycle)
         arycycles.append(bufarycycle)
-        strcycles.append(bufstrcycle)
-
-    return render_template('main.html', cycles=strcycles, name="a")
+        images.append(ImagingObjct(bufcycle, height, width).sss())
+    img.save('img.gif', save_all=True, append_images=images[1:],
+             optimize=False, duration=40, loop=0)
+    return render_template('main.html', cycles=arycycles, name="a")
 
 
 if __name__ == "__main__":
-    #app.run(debug=True)
+    # app.run(debug=True)
     # app.run('0.0.0.0',port=8080)
     hello()
